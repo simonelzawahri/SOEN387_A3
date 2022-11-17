77e386cf8f0aff899602263659a21a684e7ed3df
SimonElzawahri <simon.62@live.ca>
2022-11-17 17:45:55 -0500

    Update User.java

--- src/main/java/com/example/soen387_a2/bean/User.java
+++ src/main/java/com/example/soen387_a2/bean/User.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class User implements Serializable{
 
-
     //private static final long serialVersionUID = 1;
     private int id;
     private String firstName;
